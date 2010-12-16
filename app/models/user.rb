0a27c79463b6119d09703d8c075339924d3ee90b
Chris Irish <irish@integrumtech.com>
2010-12-15 18:10:01 -0700

    mixing of gist styles with code

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,12 @@ class User < ActiveRecord::Base
                              :url         => "/system/:class/:attachment/:id/:style/:filename"
   
   # Validations
-  validates_presence_of     :first_name, :last_name, :email
+  validates_presence_of     :first_name, :last_name, :email, :username
   validates_presence_of     :gender, :height, :weight,   :if => Proc.new { |u| u.status != 'step_one' }
   validates_inclusion_of    :gender,   :in => ['male', 'female'], :if => Proc.new { |u| u.status != 'step_one' }, :message => 'please select'
   validates_presence_of     :password, :if => Proc.new { |u| u.password_confirmation.present? }
   validates_confirmation_of :password, :if => Proc.new { |u| u.password_confirmation.present? }
-  validates_uniqueness_of   :email
+  validates_uniqueness_of   :email, :username
   validates_confirmation_of :email
   validates_length_of       :email, :in => 6..100
   validates_attachment_presence :avatar, :if => Proc.new { |u| !['step_one'].include?(u.status) }
--- config/environment.rb
+++ config/environment.rb
@@ -28,3 +28,17 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  unless html_tag.match(/type=\"hidden\"/)
+    error_class = "fieldError"
+    if html_tag =~ /<(input|textarea|select)[^>]+class=/
+      style_attribute = html_tag =~ /class=['"]/
+      html_tag.insert(style_attribute + 7, "#{error_class} ")
+    elsif html_tag =~ /<(input|textarea|select)/
+      first_whitespace = html_tag =~ /\s/
+      html_tag[first_whitespace] = " class='#{error_class}' "
+    end
+  end
+  html_tag
+end
