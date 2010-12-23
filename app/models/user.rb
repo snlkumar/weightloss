bd8b763e89e2b607e531c5a252c16b4dcb3a8f33
Chris Irish <supairish@gmail.com>
2010-12-22 17:27:11 -0700

    removed unused rails default js files, added jQuery to project, hooked up js to clear fields with errors, updated password valiation to minimum 6 charachters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ActiveRecord::Base
-  acts_as_authentic do |c|
-    c.login_field = 'email'
-  end
-  
   has_attached_file :avatar, :styles      => { :large => "50x50!" }, 
                              :url         => "/system/:class/:attachment/:id/:style/:filename"
   
+  # Auth Logic
+  acts_as_authentic do |config|
+    config.login_field = 'email'
+    config.merge_validates_length_of_password_confirmation_field_options :minimum => 6
+    config.merge_validates_length_of_password_field_options              :minimum => 6
+  end
+
   # Validations
   validates_presence_of     :first_name, :last_name, :email, :username
   validates_presence_of     :gender, :height, :weight,   :if => Proc.new { |u| u.status != 'step_one' }
@@ -14,7 +17,8 @@ class User < ActiveRecord::Base
   validates_confirmation_of :password, :if => Proc.new { |u| u.password_confirmation.present? }
   validates_uniqueness_of   :email, :username
   validates_confirmation_of :email
-  validates_length_of       :email, :in => 6..100
+  validates_length_of       :email,    :in => 6..100
+  validates_length_of       :username, :in => 4..25
   validates_attachment_presence :avatar, :if => Proc.new { |u| !['step_one'].include?(u.status) }
   
   default_values :status => "step_one"
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,24 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+jQuery.ajaxSetup({
+  'beforeSend': function(xhr) {
+    xhr.setRequestHeader("Accept", "text/javascript")
+  }
+});
+
+// Global OnReady callbacks
+$(document).ready(function() {
+
+  // Form Error fields onFocus, blank out, remove error styling
+  $(".fieldError").live('focus', function(){
+    // If a form field has a error we still want to show help messages
+    if($(this).hasClass('fieldError')){
+      if( $(this).is('input') ){ 
+        $(this).val('');
+      }
+      $(this).removeClass('fieldError');
+    }
+  });
+  
+});
