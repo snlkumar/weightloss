0958c33ccd94ebeab7b080e09b90d246500d66df
Chris Irish <supairish@gmail.com>
2010-12-22 18:16:41 -0700

    layout and style updates to step two of the sign up process

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,6 @@ class ApplicationController < ActionController::Base
 
   layout 'application' 
   
-  what is this!!!!
-  
   # Scrub sensitive parameters from your log
   filter_parameter_logging :password, :password_confirmation
   
