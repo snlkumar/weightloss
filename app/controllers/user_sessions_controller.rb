23955379f1d9928ac6036c184089e0876e2239ad
Chris Irish <supairish@gmail.com>
2011-01-11 23:57:49 -0700

    after login redirec to user's profile, removed capybara js emulation from env.rb

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -11,7 +11,7 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       flash[:notice] = "Login successful!"
-      redirect_to :controller => :users, :action => :next
+      redirect_to user_path(@user_session.user)
     else
       render :action => :new
     end
