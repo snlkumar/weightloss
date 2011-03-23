7ac4764937707733d9b6fa949750349aadbb0deb
Chris Irish <supairish@gmail.com>
2011-03-23 11:38:51 -0700

    only search against foods that have serving sizes defined

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -4,7 +4,7 @@ class FoodsController < ApplicationController
   def search
     terms  = params[:term].split(/,|\s/).reject(&:blank?)
     conds  = terms.collect{|t| "shrt_desc LIKE ?"}.join(' AND ')
-    @foods = Food.find(:all, :conditions => [conds, *terms.collect{|t| "%#{t}%"}])
+    @foods = Food.with_serving_sizes.find(:all, :conditions => [conds, *terms.collect{|t| "%#{t}%"}])
     
     render :json => @foods.map{|f| {:value => f.shrt_desc, :id => f.id} }.to_json
   end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,3 @@
 class Food < ActiveRecord::Base
+  named_scope :with_serving_sizes, :conditions => "gmwt_desc1 is NOT NULL and gmwt_desc2 is NOT NULL"
 end
