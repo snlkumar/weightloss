f41ab851cb298f21147910b417a391fd5525fbd0
Chris Irish <supairish@gmail.com>
2011-04-15 15:56:31 -0700

    fixed bug where only foods with both a gmwt_desc1/2 were shown, changed to OR

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,3 @@
 class Food < ActiveRecord::Base
-  named_scope :with_serving_sizes, :conditions => "gmwt_desc1 is NOT NULL and gmwt_desc2 is NOT NULL"
+  named_scope :with_a_serving_size, :conditions => "gmwt_desc1 is NOT NULL OR gmwt_desc2 is NOT NULL"
 end
