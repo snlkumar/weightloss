2a7818e2d600ea520ca38db263552df76c2e1852
Chris Irish <supairish@gmail.com>
2011-05-05 09:20:18 -0700

    switched the app to use Bundler

---
+++ Gemfile
@@ -0,0 +1,19 @@
+
+source :rubygems
+gem "rails", "~>2.3.11"
+
+gem 'paperclip', '~>2.3.5'
+gem 'hoptoad_notifier'
+gem 'fastercsv'
+gem 'linguistics'
+gem 'will_paginate', '2.3.11'
+gem 'authlogic', '2.1.6'
+
+group :cucumber do
+  gem 'cucumber'
+  gem 'cucumber-rails',   '>=0.3.2'
+  gem 'database_cleaner', '>=0.5.0'
+  gem 'capybara',         '>=0.4.1.1'
+  gem 'rspec',            '1.3.2'
+  gem 'rspec-rails',      '1.3.4'
+end
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.10' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -14,11 +14,7 @@ Rails::Initializer.run do |config|
   # Add additional load paths for your own custom dirs
   # config.autoload_paths += %W( #{RAILS_ROOT}/extras )
   
-  config.gem 'paperclip', :version => '2.3.5'
-  config.gem 'hoptoad_notifier'
-  config.gem 'fastercsv'
-  config.gem 'linguistics'
-  config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
+  
   
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
