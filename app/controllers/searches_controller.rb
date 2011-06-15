edc8362a46ef6cc42ed0b038ea788276dbcf1d94
Chris Irish <supairish@gmail.com>
2011-06-14 19:04:29 -0700

    work on search results page layout

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -11,6 +11,9 @@ class SearchesController < ApplicationController
     video_search = VideoSearch.new(params[:search])
     @videos      = video_search.go
     
+    @results = [@posts, @videos].flatten.sort_by{|rec| rec.created_at }
+    @total   = @posts.total_entries + @videos.total_entries
+    
     render :action => :new
   end
   
