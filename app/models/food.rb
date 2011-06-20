b00f054203f10a4b306b5d93d55984c74f0903d7
Chris Irish <supairish@gmail.com>
2011-06-20 14:52:38 -0700

    validate presence of food name, calories, and serving size

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,17 @@
 class Food < ActiveRecord::Base
   named_scope :with_a_serving_size, :conditions => "gmwt_desc1 is NOT NULL OR gmwt_desc2 is NOT NULL"
   
+  validates_presence_of :name
+  validates_uniqueness_of :name
+  
+  validate :calories_and_serving
+  
+  def calories_and_serving
+    if self.custom?
+      self.errors.add_to_base("Serving size cannot be blank.") if self.gmwt_desc1.blank?
+      self.errors.add_to_base("Calories cannot be blank.") if self.energ_kcal.blank?
+    else
+      
+    end
+  end
 end
