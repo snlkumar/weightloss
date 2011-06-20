5f4cdfcf278fa1c7183a05903e7f8632822168ee
Chris Irish <supairish@gmail.com>
2011-06-20 14:51:41 -0700

    only going to show categories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-
-  layout 'application' 
+  
+  layout 'application'
   
   # Scrub sensitive parameters from your log
   filter_parameter_logging :password, :password_confirmation
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   before_filter :set_grid
   
   def get_categories
-    @categories = Category.all + Subcategory.all
+    @categories = Category.all# + Subcategory.all
   end
   
   def set_defaults
@@ -26,7 +26,7 @@ class ApplicationController < ActionController::Base
   def require_admin
     unless current_user.admin?
       store_location
-      flash[:notice] = "You must be an admin to access this page"
+      flash[:error] = "You must be an admin to access this page"
       redirect_to root_path
       return false
     end
@@ -43,27 +43,27 @@ class ApplicationController < ActionController::Base
   def require_user
     unless current_user
       store_location
-      flash[:notice] = "You must be logged in to access this page"
+      flash[:error] = "You must be logged in to access this page"
       redirect_to new_user_session_url
       return false
     else
       @user = current_user
     end
   end
-
+  
   def require_no_user
     if current_user
       store_location
-      flash[:notice] = "You must be logged out to access this page"
+      flash[:warning] = "You must be logged out to access this page"
       redirect_to :controller => :users, :action => :next
       return false
     end
   end
-
+  
   def store_location
     session[:return_to] = request.request_uri
   end
-
+  
   def redirect_back_or_default(default)
     redirect_to(session[:return_to] || default)
     session[:return_to] = nil
@@ -74,7 +74,7 @@ class ApplicationController < ActionController::Base
       return @current_user_session if defined?(@current_user_session)
       @current_user_session = UserSession.find
     end
-
+    
     def current_user
       return @current_user if defined?(@current_user)
       @current_user = current_user_session && current_user_session.user
