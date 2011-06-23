8bd09d7be7ac191de6534e14181f5968c41463a8
Chris Irish <supairish@gmail.com>
2011-06-23 13:18:28 -0700

    added tinymce media plugin

--- config/initializers/tinymce_hammer.rb
+++ config/initializers/tinymce_hammer.rb
@@ -1,5 +1,5 @@
 
-Tinymce::Hammer.plugins = %w(tabfocus)
+Tinymce::Hammer.plugins = %w(tabfocus media)
 
 Tinymce::Hammer.init = [
   [:paste_convert_headers_to_strong, true],
@@ -10,10 +10,11 @@ Tinymce::Hammer.init = [
   [:convert_newlines_to_brs, true],
   [:remove_redundant_brs, true],
   [:theme, 'advanced'],
+  [:cleanup, true],
   [:theme_advanced_toolbar_align, 'left'],
   [:theme_advanced_toolbar_location, 'top'],
-  [:theme_advanced_buttons1, 'undo,redo,|,bold,italic,bullist,numlist,removeformat,|,link,unlink,|,code'],
+  [:theme_advanced_buttons1, 'undo,redo,|,bold,italic,bullist,numlist,removeformat,|,link,unlink,media,video,|,code'],
   [:theme_advanced_buttons2, ''],
   [:theme_advanced_buttons3, ''],
-  [:valid_elements, "a[href|title],blockquote[cite],br,caption,cite,code,dl,dt,dd,em,i,img[src|alt|title|width|height|align],li,ol,p,pre,q[cite],small,strike,strong/b,sub,sup,u,ul"]
+  [:valid_elements, "a[name|href|title],blockquote[cite],br,caption,cite,code,dl,dt,dd,em,i,img[src|alt|title|width|height|align],li,ol,p,pre,q[cite],small,strike,strong/b,sub,sup,u,ul,param[name|value],embed[src|type|wmode|width|height],object[classid|codebase|id|name|type|allowScriptAccess|allowNetworking|height|width|data]"]
 ]
