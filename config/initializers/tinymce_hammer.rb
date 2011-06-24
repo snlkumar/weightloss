4425c2c501e05554d66983f7df1985c231ac27f3
Chris Irish <supairish@gmail.com>
2011-06-24 01:43:11 -0700

    more tinymce allowed elements

--- config/initializers/tinymce_hammer.rb
+++ config/initializers/tinymce_hammer.rb
@@ -16,5 +16,5 @@ Tinymce::Hammer.init = [
   [:theme_advanced_buttons1, 'undo,redo,|,bold,italic,bullist,numlist,removeformat,|,link,unlink,media,video,|,code'],
   [:theme_advanced_buttons2, ''],
   [:theme_advanced_buttons3, ''],
-  [:valid_elements, "a[name|href|title],blockquote[cite],br,caption,cite,code,dl,dt,dd,em,i,img[src|alt|title|width|height|align],li,ol,p,pre,q[cite],small,strike,strong/b,sub,sup,u,ul,param[name|value],embed[src|type|wmode|width|height],object[classid|codebase|id|name|type|allowScriptAccess|allowNetworking|height|width|data]"]
+  [:valid_elements, "a[name|href|title],blockquote[cite],br,caption,cite,code,dl,dt,dd,em,i,img[src|alt|title|width|height|align],li,ol,p,pre,q[cite],small,strike,strong/b,sub,sup,u,ul,param[name|value|_value],embed[type|width|height|src|*],object[classid|width|height|codebase|*]"]
 ]
