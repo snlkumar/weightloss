9226e814c3557402b22acee15aa3f6029f1d5983
Chris Irish <supairish@gmail.com>
2011-07-07 13:59:57 -0700

    fixed date population on workout form, fixed style that was hiding exercises when editing a workout

--- app/helpers/exercises_helper.rb
+++ app/helpers/exercises_helper.rb
@@ -1,7 +1,13 @@
 module ExercisesHelper
   
-  def trained_on_start_date
-    params[:date] ? params[:date].to_date.strftime("%m/%d/%Y") : Date.today.strftime("%m/%d/%Y")
+  def trained_on_start_date(obj)
+    if params[:date]
+      params[:date].to_date.strftime("%m/%d/%Y")
+    elsif obj
+      obj.trained_on.strftime("%m/%d/%Y")
+    else
+      Date.today.strftime("%m/%d/%Y")
+    end
   end
   
 end
