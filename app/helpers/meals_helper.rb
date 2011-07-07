df8d1514e0bfb477cfd8760e7e3a9a313431758f
Chris Irish <supairish@gmail.com>
2011-07-07 13:55:42 -0700

    when editing a meal date was being populated incorrectly, same with meal type

--- app/helpers/meals_helper.rb
+++ app/helpers/meals_helper.rb
@@ -1,7 +1,13 @@
 module MealsHelper
   
-  def ate_on_start_date
-    params[:date] ? params[:date].to_date.strftime("%m/%d/%Y") : Date.today.strftime("%m/%d/%Y")
+  def ate_on_start_date(obj)
+    if params[:date]
+      params[:date].to_date.strftime("%m/%d/%Y")
+    elsif obj
+      obj.ate_on.strftime("%m/%d/%Y")
+    else
+      Date.today.strftime("%m/%d/%Y")
+    end
   end
   
 end
