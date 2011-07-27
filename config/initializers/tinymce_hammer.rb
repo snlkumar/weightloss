32f3830e7af0deee93b9fa9cc96acbe27350c1b9
Chris Irish <supairish@gmail.com>
2011-07-27 16:25:36 -0700

    added ability to float post images left or right in content

--- config/initializers/tinymce_hammer.rb
+++ config/initializers/tinymce_hammer.rb
@@ -14,5 +14,5 @@ Tinymce::Hammer.init = [
   [:theme_advanced_buttons1, 'undo,redo,|,bold,italic,bullist,numlist,removeformat,|,link,unlink,image,media,video,|,code'],
   [:theme_advanced_buttons2, ''],
   [:theme_advanced_buttons3, ''],
-  [:valid_elements, "a[name|href|title],blockquote[cite],br,caption,cite,code,dl,dt,dd,em,i,img[src|alt|title|width|height|align],li,ol,p,pre,q[cite],small,strike,strong/b,sub,sup,u,ul,param[name|value|_value],embed[type|width|height|src|*],object[classid|width|height|codebase|*]"]
+  [:valid_elements, "a[name|href|title],blockquote[cite],br,caption,cite,code,dl,dt,dd,em,i,img[src|alt|title|width|height|align|style],li,ol,p,pre,q[cite],small,strike,strong/b,sub,sup,u,ul,param[name|value|_value],embed[type|width|height|src|*],object[classid|width|height|codebase|*],style"]
 ]
