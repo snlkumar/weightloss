969d57c048ece76697b194e53de3f98b17890c24
Chris Irish <supairish@gmail.com>
2011-08-11 11:04:23 -0700

    updated spec_helper with snippet from Jo Liss, to switch between transaction/truncation depending on whether were testing JS or not

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,12 +13,17 @@ DatabaseCleaner.strategy = :truncation
 RSpec.configure do |config|
   config.mock_with :rspec
   config.use_transactional_fixtures = false
-  
-  
-  config.before :each do
-    DatabaseCleaner.start
-  end
-  config.after :each do
-    DatabaseCleaner.clean
-  end
+
+    config.before :each do
+      if Capybara.current_driver == :rack_test
+        DatabaseCleaner.strategy = :transaction
+      else
+        DatabaseCleaner.strategy = :truncation
+      end
+      DatabaseCleaner.start
+    end
+
+    config.after do
+      DatabaseCleaner.clean
+    end
 end
