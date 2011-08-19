3dad8168290112e6e82cebb475b531459810419e
Chris Irish <supairish@gmail.com>
2011-08-19 02:05:22 -0700

    log omniauth.auth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,6 +8,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.facebook_data"] = env["omniauth.auth"]
+      Rails.logger.warn env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
