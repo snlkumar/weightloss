9336c28f259cc8325f49a4c0d0321d330d5918c1
ravi <ravi@ravi-ThinkPad-Edge-E430.(none)>
2013-04-05 19:13:13 +0530

    forem posts hide 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130405061459) do
+ActiveRecord::Schema.define(:version => 20130405133937) do
 
   create_table "City", :primary_key => "ID", :force => true do |t|
     t.string "Name",        :limit => 35, :default => "", :null => false
